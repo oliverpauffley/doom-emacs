b0d694b7b20cacb63586595b4ee11c23764206b3
Ollie <mrpauffley@gmail.com>
2022-01-24 10:32:21 +0000

    fix: nix-lsp

--- config.el
+++ config.el
@@ -196,11 +196,8 @@
       vterm-shell "zsh")
 
 ;; nix lsp
-(add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
-                  :major-modes '(nix-mode)
-                  :server-id 'nix))
+(add-hook 'nix-mode-hook #'lsp)
+
 ;; EXWM bindings
 (defun my-exwm-launch (command)
   (lambda ()
