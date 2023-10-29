827acdd754a9457d97aa6514be8f5037d8929ac8
oliverpauffley <mrpauffley@gmail.com>
2023-10-29 20:48:01 +0000

    merge new laptop

--- functions.el
+++
@@ -1,9 +0,0 @@
-;;; functions.el -*- lexical-binding: t; -*-
-
-;; TODO get this to work
-;; seems to be an issue around the =parse-iso8601= even though it says it should work
-(defun oll/get-unix-timestamp(time)
-  "Converts the given time to a unixtimestamp in seconds since the epoch."
-  (interactive "sTime: ")
-  (insert (prin1-to-string (time-convert (parse-iso8601-time-string time) 'integer)))
-  )
